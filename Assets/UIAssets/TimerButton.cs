0a0d23d48402d04747b575905ec977dd4dbedf53
Scott Hickmann <scotthickmann21@gmail.com>
2025-01-21 14:22:00 -0800

    Add button with timer

---
+++ Assets/UIAssets/TimerButton.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerButton : MonoBehaviour
+{
+    [Tooltip("The duration of the timer in seconds")]
+    public float duration = 10.0f;
+    public TMP_Text timerText;
+    public Image fill;
+    public Button button;
+
+    private float timeLeft;
+    private bool isRunning = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.text = timeLeft.ToString("F0");
+            fill.fillAmount = timeLeft / duration;
+            if (timeLeft <= 0)
+            {
+                isRunning = false;
+                button.interactable = true;
+                timerText.text = "0";
+                fill.fillAmount = 0.0f;
+            }
+        }
+    }
+
+    public void OnClick()
+    {
+        if (isRunning)
+        {
+            return;
+        }
+        timeLeft = duration;
+        isRunning = true;
+        button.interactable = false;
+    }
+}
