a78de9fd95cb3595de79ebf252fe9050cf863639
MostlyHarmless <sja4201@gmail.com>
2025-02-25 23:26:06 -0800

    Revert Reorginization because Unity Hates Filesystems
    This reverts commit 44efbe63711194208ad375980da5f5aac5fcfb3e.
    
    Revert "Reorg stage 1"
    
    This reverts commit 2d37ce5c4527609959b839d2dd594067be8838d5.
    
    Revert "Reorg stage 2"
    
    This reverts commit fcc322975fb0661c51f6e7ab24e4ffe9db9294c6.
    
    Revert "Power up updates"
    
    This reverts commit 1f68e4a69909030889df643022a5bd316baf5739.
    
    Revert "Reorg"
    
    This reverts commit ba6d9a98c28537b1a64f2ebe1841fcd0c0f354ec.
    
    Revert "Enemy Reorg"
    
    This reverts commit 0859ed7564edd102a8dfcfadc47c4826edc673cb.

--- Assets/PlayerAssets/PlayerMovement.cs
+++ Assets/PlayerAssets/PlayerMovement.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    float bonus = 0f;
-    float moveBonusTime = 0f;
     public Rigidbody2D rb;
     public Camera cam;
     public Joystick movementJoystick;
@@ -55,23 +53,11 @@ public class PlayerMovement : MonoBehaviour
             rb = GetComponent<Rigidbody2D>();
         }
 
-        rb.AddForce(movement.normalized * (moveSpeed + bonus));
+        rb.AddForce(movement.normalized * moveSpeed);
 
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
 
         rb.rotation = angle;
-
-
-        if (bonus > 0)
-        {
-            bonus = bonus - (float)Math.Pow(3, moveBonusTime - 10f);
-            moveBonusTime += 0.1f;
-        }
-        if (bonus <= 0)
-        {
-            bonus = 0;
-            moveBonusTime = 0;
-        }
     }
 
     public void Slow(float slowAmount, string slowID, float slowDuration)
@@ -85,11 +71,6 @@ public class PlayerMovement : MonoBehaviour
             moveSpeed = moveSpeed * slowAmount;
             StartCoroutine(RemoveSlow(slowAmount, slowID, slowDuration));
         } 
-    }
-
-    public void SetSpeedBonus(float amount)
-    {
-        bonus = amount;
     }
 
     IEnumerator RemoveSlow(float slowAmount, string slowID, float slowDuration)
