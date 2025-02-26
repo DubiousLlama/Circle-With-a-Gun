24fb4e1b5c2250e606b5486a0d91ab0da49bded6
MostlyHarmless <sja4201@gmail.com>
2025-02-26 00:03:20 -0800

    Polish changes to speed bonuses

--- Assets/PlayerAssets/PlayerMovement.cs
+++ Assets/PlayerAssets/PlayerMovement.cs
@@ -22,30 +22,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get input from the joysticks or keyboard/mouse and set the movement and look direction vectors accordingly
+        HandlePlayerInput();
 
-        rb = GetComponent<Rigidbody2D>();
-
-        if (movementJoystick.Horizontal != 0 || movementJoystick.Vertical != 0)
-        {
-            movement.x = movementJoystick.Horizontal;
-            movement.y = movementJoystick.Vertical;
-        }
-        else
-        {
-            movement.x = Input.GetAxisRaw("Horizontal");
-            movement.y = Input.GetAxisRaw("Vertical");
-        }
-
-        if (directionJoystick.Horizontal != 0 || directionJoystick.Vertical != 0)
-        {
-            lookDir.x = directionJoystick.Horizontal;
-            lookDir.y = directionJoystick.Vertical;
-        }
-        else
-        {
-            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            lookDir = mousePos - rb.position;
-        }
+        // If the player has a speed bonus, decay it over time and shrink the player while the bonus is active
+        HandleSpeedBonusDecay();
     }
 
     void FixedUpdate()
@@ -61,18 +42,6 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
 
         rb.rotation = angle;
-
-
-        if (bonus > 0)
-        {
-            bonus = bonus - (float)Math.Pow(3, moveBonusTime - 10f);
-            moveBonusTime += 0.1f;
-        }
-        if (bonus <= 0)
-        {
-            bonus = 0;
-            moveBonusTime = 0;
-        }
     }
 
     public void Slow(float slowAmount, string slowID, float slowDuration)
@@ -99,5 +68,52 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(slowDuration);
         slowIDs.Remove(slowID);
         moveSpeed = moveSpeed * (1 / slowAmount);
+    }
+
+    private void HandleSpeedBonusDecay()
+    {
+        if (bonus > 0)
+        {
+            bonus = bonus - (float)Math.Pow(2, moveBonusTime - 12f);
+            moveBonusTime += Time.deltaTime;
+
+            // Shrink the player by the ratio of the bonus to their moveSpeed
+            float bonusRatio = Mathf.Min(bonus / moveSpeed, 1);
+            float scale = (1 - bonusRatio / 4) * 0.4f;
+            transform.localScale = new Vector3(scale, scale, 1);
+        }
+        if (bonus <= 0)
+        {
+            bonus = 0;
+            moveBonusTime = 0;
+            transform.localScale = new Vector3(0.4f, 0.4f, 1);
+        }
+    }
+
+    private void HandlePlayerInput()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (movementJoystick.Horizontal != 0 || movementJoystick.Vertical != 0)
+        {
+            movement.x = movementJoystick.Horizontal;
+            movement.y = movementJoystick.Vertical;
+        }
+        else
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        if (directionJoystick.Horizontal != 0 || directionJoystick.Vertical != 0)
+        {
+            lookDir.x = directionJoystick.Horizontal;
+            lookDir.y = directionJoystick.Vertical;
+        }
+        else
+        {
+            Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            lookDir = mousePos - rb.position;
+        }
     }
 }
